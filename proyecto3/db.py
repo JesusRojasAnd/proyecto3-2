2aab6c41ea676b263f39c6c3496ade9bc1a09c73
Jesus <jesus.rojas.andrade.alu@iesjulioverne>
2026-01-29 09:39:11 +0100

    ewgewg

--- proyecto3/db.py
+++ proyecto3/db.py
@@ -1,16 +1,5 @@
 import os
+import dj_database_url
 MYSQL = {
-'default': {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
-}
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
