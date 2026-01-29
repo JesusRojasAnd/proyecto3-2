4bf50eea0eea41054bacead66a66b4b446081a25
Jesus <jesus.rojas.andrade.alu@iesjulioverne>
2026-01-29 09:41:43 +0100

    dadadada

--- proyecto3/db.py
+++ proyecto3/db.py
@@ -1,5 +1,5 @@
 import os
 import dj_database_url
 MYSQL = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get('mysql://root:TRFCqihGtyBhmBNCBctCKQAKgwAtJNAm@mysql.railway.internal:3306/railway'))
 }
