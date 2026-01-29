6b84fa107de22d57983acd273f5944075cd2e4bd
Jesus <jesus.rojas.andrade.alu@iesjulioverne>
2026-01-29 09:05:22 +0100

    asd

--- proyecto3/settings.py
+++ proyecto3/settings.py
@@ -127,4 +127,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-CRISPY_TEMPLATE_PACK='bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
